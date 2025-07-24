ed83109a689f077eba4de4948bffbe2348a255bf
ownssec <ownssec@gmail.com>
2025-07-25 00:28:57 +0800

    update added a workspace

---
+++ workspace_nav.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+direction=$1
+current=$(i3-msg -t get_workspaces | jq '.[] | select(.focused).num')
+all_workspaces=($(i3-msg -t get_workspaces | jq -r '.[].num' | sort -n))
+
+if [[ "$direction" == "left" ]]; then
+  target=$(echo "${all_workspaces[@]}" | tr ' ' '\n' | awk -v curr="$current" '$1 < curr {print $1}' | tail -1)
+elif [[ "$direction" == "right" ]]; then
+  target=$(echo "${all_workspaces[@]}" | tr ' ' '\n' | awk -v curr="$current" '$1 > curr {print $1}' | head -1)
+fi
+
+[[ -n "$target" ]] && i3-msg workspace number "$target"
